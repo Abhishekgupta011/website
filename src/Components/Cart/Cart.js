4ab249f22db52c1e102a7dbc73a2d1566b6b11da
Abhishekgupta011 <ag25061999@gmail.com>
2023-07-17 22:21:31 +0530

    restaurant website

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -2,14 +2,31 @@ import React, { useContext } from "react";
 import './Cart.css'
 import Modal from "../UI/Modal";
 import CartContext from "../Store/CartContext";
-
+import CartItem from "./CartItem";
 const Cart = (props) => {
+
     const cartCtx = useContext(CartContext)
     const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`
+
+    const hasItems = cartCtx.items.length>0
+
+    const addItemsHandler = ()=>{}
+
+    const removeItemsHandler =(id)=>{
+        cartCtx.removeItem(id)
+    }
+
     const CartItems = (
         <ul className="cartItems">
            {cartCtx.items.map((item) =>(
-             <li>{item.name}</li>
+             <CartItem 
+             key={item.id} 
+             name = {item.name} 
+             amount={item.amount} 
+             price={item.price}
+             onAdd = {addItemsHandler.bind(null , item.item)}
+             onRemove = {removeItemsHandler.bind(null , item.id)}
+             />
            ))}
         </ul>
     )
@@ -23,7 +40,7 @@ const Cart = (props) => {
         </div>
         <div className="actions">
             <button className="close" onClick={props.onClose}>Close</button>
-            <button className="order">Order</button>
+            {hasItems && <button className="order">Order</button>}
         </div>
     </Modal>
    )
---
+++ src/Components/Cart/CartItem.js
@@ -0,0 +1,23 @@
+import React from "react";
+import './CartItem.css'
+const CartItem = (props) => {
+  const price = `$${props.price.toFixed(2)}`;
+
+  return (
+    <li className="cartItem">
+      <div>
+        <h3>{props.name}</h3>
+        <div className="itemCount">
+          <span className="price">{price}</span>
+          <span className="amount">x {props.amount}</span>
+        </div>
+      </div>
+      <div>
+        <button onClick={props.onAdd} className="adds">+</button>
+        <button onClick={props.onRemove}>-</button>
+      </div>
+    </li>
+  );
+};
+
+export default CartItem;
